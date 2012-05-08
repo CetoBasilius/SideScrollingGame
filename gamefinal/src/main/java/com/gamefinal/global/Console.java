607ad65f411557c02951de9f89cf75e617396955
bgerman <Cetobasilius@users.noreply.github.com>
2012-05-08 14:35:40 +0000

    updated input recorder

--- gamefinal/src/main/java/com/gamefinal/global/Console.java
+++ gamefinal/src/main/java/com/gamefinal/global/Console.java
@@ -2,17 +2,28 @@ package com.gamefinal.global;
 
 public class Console {
 	
+	private static final int MAX_MESSAGE_LENGHT = 24;
 	private static final int MAX_CONSOLE_LINES = 5;
 	private String consoleLines[] = new String[MAX_CONSOLE_LINES];
 	
-	public Console(){
+	private int positionX;
+	private int positionY;
+	
+	public Console(int x,int y){
 		for(int line=0;line<MAX_CONSOLE_LINES;line++){
 			consoleLines[line]="";
 		}
+		positionX = x;
+		positionY = y;
 	}
 
 	public void addMessage(String message){
 		if(message.length()>0){
+			
+			/*if(message.length()>MAX_MESSAGE_LENGHT){
+				message = message.substring(0, MAX_MESSAGE_LENGHT);
+			}*/
+			
 			for(int a = MAX_CONSOLE_LINES-1;a>0;a--){
 				consoleLines[a]=consoleLines[a-1];
 			}
@@ -22,6 +33,22 @@ public class Console {
 
 	public String getLine(int line) {
 		return consoleLines[line];
+	}
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
 	}
 
 }
